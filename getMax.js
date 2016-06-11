30352dec4e382e7fdd0f943216b65a49d6828a2a
charlieperson <charlie.person@hyperisland.se>
2016-06-11 13:47:40 +0200

    Refactors to make solution one less line

--- getMax.js
+++ getMax.js
@@ -1,5 +1,4 @@
 exports.getMax = function(num){
   var digits = num.toString().split('').map(Number);
-  var sorted = digits.sort(function (a, b) {  return b - a;  });
-  return sorted.join('');
+  return digits.sort(function (a, b) {  return b - a;  }).join('');
 };
