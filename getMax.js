b93fdde82a2bfab9cb3f6c59c802f032f22c508e
charlieperson <charlie.person@hyperisland.se>
2016-06-11 15:01:10 +0200

    Passes all tests except for edge cases

--- getMax.js
+++ getMax.js
@@ -1,4 +1,4 @@
 exports.getMax = function(num){
   var digits = num.toString().split('').map(Number);
-  return digits.sort(function (a, b) {  return b - a;  }).join('');
+  return +digits.sort(function (a, b) {  return b - a;  }).join('');
 };
